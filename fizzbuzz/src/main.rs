7b9053bc640320cdb9cdc7063ca6965154a503a5
Andres Pedraza <andrespedca@gmail.com>
2021-10-25 12:19:29 -0500

    Pruebas anadidas

--- fizzbuzz/src/main.rs
+++ fizzbuzz/src/main.rs
@@ -10,6 +10,28 @@ fn fizzbuzz (i: u32) -> String {
     }
 }
 
+#[test]
+fn fizzbuzz_test_divisores_tres () {
+    assert_eq!(fizzbuzz (3), "Fizz");
+    assert_eq!(fizzbuzz (6), "Fizz");
+    assert_eq!(fizzbuzz (9), "Fizz");
+    assert_eq!(fizzbuzz (12), "Fizz");
+}
+
+#[test]
+fn fizzbuzz_test_divisores_cinco () {
+    assert_eq!(fizzbuzz (5), "Buzz");
+    assert_eq!(fizzbuzz (10), "Buzz");
+    assert_eq!(fizzbuzz (20), "Buzz");
+}
+
+#[test]
+fn fizzbuzz_test_divisores () {
+    assert_eq!(fizzbuzz (90), "FizzBuzz");
+    assert_eq!(fizzbuzz (75), "FizzBuzz");
+    assert_eq!(fizzbuzz (60), "FizzBuzz");
+}
+
 fn main() {
     for i in 1..=100 {
         println!("{}", fizzbuzz(i));
---
+++ fizzbuzz/target/debug/deps/fizzbuzz-bea6c8d7f20b3bf8.d
@@ -0,0 +1,5 @@
+/home/andresp/LearningRust/fizzbuzz/target/debug/deps/fizzbuzz-bea6c8d7f20b3bf8: src/main.rs
+
+/home/andresp/LearningRust/fizzbuzz/target/debug/deps/fizzbuzz-bea6c8d7f20b3bf8.d: src/main.rs
+
+src/main.rs:
